9ae578515ce4b7546ccad59b11ebc871bd254a2d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2020-01-10 16:59:36 -0300

    home page completed

--- app/js/navIconColor.js
+++ app/js/navIconColor.js
@@ -1,16 +1,12 @@
-// // Change nav-icon color on click
-// const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll(".nav-link");
 
-// navLink.forEach(link => {
-//   // Listen for click
-//   link.addEventListener("click", () => {
-//     // Removes color from any active icon
-//     const navLinkNew = document.querySelectorAll(".nav-link");
-//     navLinkNew.forEach(linkNew => {
-//       linkNew.classList.remove("page-active");
-//     });
+navLink.forEach(link => {
+  link.addEventListener("click", () => {
+    navLink.forEach(link => {
+      link.classList.remove("page-active");
+    });
 
-//     // Add color to new active icon
-//     link.classList.add("page-active");
-//   });
-// });
+    !link.classList.contains("page-active") &&
+      link.classList.add("page-active");
+  });
+});
--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,1 @@
-const navLink=document.querySelectorAll(".nav-link");navLink.forEach(link=>{link.addEventListener("click",()=>{document.querySelectorAll(".nav-link").forEach(link=>{link.classList.remove("page-active")}),link.classList.add("page-active")})});
+const navLink=document.querySelectorAll(".nav-link");navLink.forEach(link=>{link.addEventListener("click",()=>{navLink.forEach(link=>{link.classList.remove("page-active")}),!link.classList.contains("page-active")&&link.classList.add("page-active")})});
